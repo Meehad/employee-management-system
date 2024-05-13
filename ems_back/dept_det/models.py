e29bd370b289b552053a9fa1355687f28d282a68
Meehad <mohammedmihad2@gmail.com>
2024-05-14 02:54:29 +0530

    fixes

--- ems_back/dept_det/models.py
+++ ems_back/dept_det/models.py
@@ -1,12 +1,11 @@
 from django.db import models
-from emp_details.models import *
 
 # Department object or schema
 class Department(models.Model):
-    man_id = models.ForeignKey(Employee, to_field='emp_id',on_delete=models.CASCADE,limit_choices_to={'role': 'M'})
-    dep_name=models.CharField(max_length=100,unique=True)
-    dep_location=models.CharField(max_length=100,unique=True)
-    dept_id=models.IntegerField(unique=True)
-    
-    def __str__(self) :
-        return self.man_id
+    dep_name = models.CharField(max_length=100, unique=True)
+    dep_location = models.CharField(max_length=100, unique=True)
+    dept_id = models.IntegerField(unique=True)
+    man_id = models.ForeignKey('emp_details.Employee', to_field='emp_id', on_delete=models.CASCADE, limit_choices_to={'role': 'M'})
+
+    def __str__(self):
+        return self.dep_name
